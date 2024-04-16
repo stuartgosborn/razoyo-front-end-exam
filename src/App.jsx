726c70347a5039ba1231618d126cc6de6c12c60d
Stuart Osborn <stuartgosborn22@gmail.com>
2024-04-15 22:51:59 -0500

    added fetch promise for getting car list data

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState, useEffect } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+
+  // token for authorization
+  const token =
+    "SFMyNTY.g2gDbQAAABI6OmZmZmY6MTY5LjI1NC4xLjFuBgCL8pDkjgFiAAFRgA.KM4kFCCoLzV8a-CfiwUfKaXKgBal2NT4Sp6iEL-id1U";
+
+  // url to fetch car list
+  const carListUrl = `https://exam.razoyo.com/api/cars`;
+
+  // make call to fetch car list
+
+  try {
+    fetch(carListUrl)
+      .then((response) => {
+        console.log(response);
+        return response.json();
+      })
+      .then((data) => {
+        console.log(data);
+      });
+  } catch (error) {
+    console.error("Error:", error);
+  }
 
   return (
     <>
@@ -16,7 +38,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1></h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
@@ -29,7 +51,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
