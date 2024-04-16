dc378ee21b2a3f76b4da89a3c37b54abb1b234c5
Stuart Osborn <stuartgosborn22@gmail.com>
2024-04-16 11:58:23 -0500

    installed dependencies for material UI Grid

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,13 @@ import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
+  const [carList, setCarList] = useState([]);
+  const [makeList, setMakeList] = useState([]);
 
   // token for authorization
+
+  useEffect(() => {
+
   const token =
     "SFMyNTY.g2gDbQAAABI6OmZmZmY6MTY5LjI1NC4xLjFuBgCL8pDkjgFiAAFRgA.KM4kFCCoLzV8a-CfiwUfKaXKgBal2NT4Sp6iEL-id1U";
 
@@ -23,33 +28,22 @@ function App() {
       })
       .then((data) => {
         console.log(data);
+        setCarList(data.cars);
+        console.log(carList);
+        setMakeList(data.makes);
+        console.log(makeList);
       });
   } catch (error) {
     console.error("Error:", error);
   }
+  }, []);
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+
       </div>
-      <h1></h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    
     </>
   );
 }
